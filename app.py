304e70b1f3fdde5783bdff7cb15a316a46d035c8
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2025-01-06 20:59:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,44 +54,51 @@ def ljsynthesize(text):
 with gr.Blocks() as vctk: # just realized it isn't vctk but libritts but i'm too lazy to change it rn
     with gr.Row():
         with gr.Column(scale=1):
-            inp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
-            voice = gr.Dropdown(voicelist, label="Giọng nói", info="Chọn giọng nói mặc định.", value='m-us-2', interactive=True)
+            inp = gr.Textbox(label="Văn bản", info="Bạn muốn đọc gì? Công cụ AI này hoạt động tốt hơn với các câu dài.", interactive=True)
+            voice = gr.Dropdown(voicelist, label="Giọng nói", info="Chọn giọng nói tham khảo", value='m-us-2', interactive=True)
             multispeakersteps = gr.Slider(minimum=5, maximum=15, value=7, step=1, label="Số bước khuếch tán", info="Càng cao thì chất lượng càng tốt, nhưng chậm hơn", interactive=True)
         with gr.Column(scale=1):
-            btn = gr.Button("Tổng hợp giọng nói", variant="primary")
-            audio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
+            btn = gr.Button("Tạo giọng nói", variant="primary")
+            audio = gr.Audio(interactive=False, label="Kết quả")
             btn.click(synthesize, inputs=[inp, voice, multispeakersteps], outputs=[audio], concurrency_limit=4)
 with gr.Blocks() as clone:
     with gr.Row():
         with gr.Column(scale=1):
-            clinp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
+            clinp = gr.Textbox(label="Văn bản", info="Bạn muốn đọc gì? Công cụ AI này hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
             clvoice = gr.Audio(label="Giọng nói", interactive=True, type='filepath', max_length=300)
-            vcsteps = gr.Slider(minimum=5, maximum=30, value=20, step=1, label="Số bước khuếch tán", info="Càng cao thì chất lượng càng tốt, nhưng chậm hơn", interactive=True)
+            vcsteps = gr.Slider(minimum=5, maximum=40, value=20, step=1, label="Số bước lặp AI", info="Càng cao thì chất lượng càng tốt, nhưng chậm hơn", interactive=True)
         with gr.Column(scale=1):
-            clbtn = gr.Button("Tổng hợp giọng nói", variant="primary")
-            claudio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
+            clbtn = gr.Button("Tạo giọng nói", variant="primary")
+            claudio = gr.Audio(interactive=False, label="Kết quả")
             clbtn.click(clsynthesize, inputs=[clinp, clvoice, vcsteps], outputs=[claudio], concurrency_limit=4)
 with gr.Blocks() as lj:
     with gr.Row():
         with gr.Column(scale=1):
-            ljinp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
+            ljinp = gr.Textbox(label="Văn bản", info="Bạn muốn đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
         with gr.Column(scale=1):
-            ljbtn = gr.Button("Tổng hợp giọng nói", variant="primary")
-            ljaudio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
+            ljbtn = gr.Button("Tạo giọng nói", variant="primary")
+            ljaudio = gr.Audio(interactive=False, label="Kết quả")
             ljbtn.click(ljsynthesize, inputs=[ljinp], outputs=[ljaudio], concurrency_limit=4)
-with gr.Blocks(title="StyleTTS 2", css="footer{display:none !important}", theme=theme) as demo:
-    gr.Markdown("""# StyleTTS 2
+with gr.Blocks(title="VNUIS IDT - Công cụ AI sao chép giọng nói StyleTTS 2", css="footer{display:none !important}", theme=theme) as demo:
+    gr.Markdown("""# VNUIS IDT - Công cụ AI sao chép giọng nói StyleTTS 2
 
-[Paper](https://arxiv.org/abs/2306.07691) - [Samples](https://styletts2.github.io/) - [Code](https://github.com/yl4579/StyleTTS2) - [Interface](https://twitter.com/realmrfakename)
+Triển khai bởi Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội
 
-Dịch và Triển khai bởi Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội
+**Cảnh báo quan trọng**
 
-**Before using this demo, you agree to inform the listeners that the speech samples are synthesized by the pre-trained models, unless you have the permission to use the voice you synthesize. That is, you agree to only use voices whose speakers grant the permission to have their voice cloned, either directly or by license before making synthesized voices public, or you have to publicly announce that these voices are synthesized if you do not have the permission to use these voices.**
+Công cụ AI này chỉ dành cho việc tổng hợp giọng nói cá nhân của bạn. Bạn hoàn toàn chịu trách nhiệm về việc sử dụng công cụ này.
 
-**LƯU Ý: StyleTTS 2 hoạt động tốt hơn với các đoạn văn bản dài.** Ví dụ, yêu cầu nói "Hello" sẽ cho ra kết quả chất lượng thấp hơn so với việc yêu cầu một cụm từ dài hơn.""")
-    gr.TabbedInterface([vctk, clone, lj], ['Nhiều giọng nói', 'Sao chép giọng nói', 'LJSpeech'])
+**Lưu ý:**
+
+1. Bạn không được phép sử dụng giọng nói của người khác để sao chép hoặc tạo giọng nói mà không có sự cho phép.
+2. Bất kỳ hành vi vi phạm nào liên quan đến quyền riêng tư hoặc bản quyền sẽ do bạn chịu trách nhiệm trước pháp luật.
+
+**Công cụ AI này hoạt động tốt hơn với các đoạn văn bản dài.** Ví dụ, yêu cầu nói "Hello" sẽ cho ra kết quả chất lượng thấp hơn so với việc yêu cầu một đoạn dài.""")
+    gr.TabbedInterface([clone], ['Sao chép giọng nói'])
     gr.Markdown("""
-Demo bởi [mrfakename](https://twitter.com/realmrfakename). Dịch và Triển khai bởi Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội
+[Paper](https://arxiv.org/abs/2306.07691) - [Samples](https://styletts2.github.io/) - [Code](https://github.com/yl4579/StyleTTS2) - [Interface](https://twitter.com/realmrfakename).
+
+**Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội**
 """)
 if __name__ == "__main__":
     demo.queue(api_open=False, max_size=15).launch(
