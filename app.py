4653893c660d4c8169468802f476313855adae78
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2025-01-06 12:46:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,49 +54,44 @@ def ljsynthesize(text):
 with gr.Blocks() as vctk: # just realized it isn't vctk but libritts but i'm too lazy to change it rn
     with gr.Row():
         with gr.Column(scale=1):
-            inp = gr.Textbox(label="Text", info="What would you like StyleTTS 2 to read? It works better on full sentences.", interactive=True)
-            voice = gr.Dropdown(voicelist, label="Voice", info="Select a default voice.", value='m-us-2', interactive=True)
-            multispeakersteps = gr.Slider(minimum=5, maximum=15, value=7, step=1, label="Diffusion Steps", info="Higher = better quality, but slower", interactive=True)
-            # use_gruut = gr.Checkbox(label="Use alternate phonemizer (Gruut) - Experimental")
+            inp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
+            voice = gr.Dropdown(voicelist, label="Giọng nói", info="Chọn giọng nói mặc định.", value='m-us-2', interactive=True)
+            multispeakersteps = gr.Slider(minimum=5, maximum=15, value=7, step=1, label="Số bước khuếch tán", info="Càng cao thì chất lượng càng tốt, nhưng chậm hơn", interactive=True)
         with gr.Column(scale=1):
-            btn = gr.Button("Synthesize", variant="primary")
-            audio = gr.Audio(interactive=False, label="Synthesized Audio")
+            btn = gr.Button("Tổng hợp giọng nói", variant="primary")
+            audio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
             btn.click(synthesize, inputs=[inp, voice, multispeakersteps], outputs=[audio], concurrency_limit=4)
 with gr.Blocks() as clone:
     with gr.Row():
         with gr.Column(scale=1):
-            clinp = gr.Textbox(label="Text", info="What would you like StyleTTS 2 to read? It works better on full sentences.", interactive=True)
-            clvoice = gr.Audio(label="Voice", interactive=True, type='filepath', max_length=300)
-            vcsteps = gr.Slider(minimum=5, maximum=20, value=20, step=1, label="Diffusion Steps", info="Higher = better quality, but slower", interactive=True)
+            clinp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
+            clvoice = gr.Audio(label="Giọng nói", interactive=True, type='filepath', max_length=300)
+            vcsteps = gr.Slider(minimum=5, maximum=30, value=20, step=1, label="Số bước khuếch tán", info="Càng cao thì chất lượng càng tốt, nhưng chậm hơn", interactive=True)
         with gr.Column(scale=1):
-            clbtn = gr.Button("Synthesize", variant="primary")
-            claudio = gr.Audio(interactive=False, label="Synthesized Audio")
+            clbtn = gr.Button("Tổng hợp giọng nói", variant="primary")
+            claudio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
             clbtn.click(clsynthesize, inputs=[clinp, clvoice, vcsteps], outputs=[claudio], concurrency_limit=4)
 with gr.Blocks() as lj:
     with gr.Row():
         with gr.Column(scale=1):
-            ljinp = gr.Textbox(label="Text", info="What would you like StyleTTS 2 to read? It works better on full sentences.", interactive=True)
+            ljinp = gr.Textbox(label="Văn bản", info="Bạn muốn StyleTTS2 đọc gì? StyleTTS2 hoạt động tốt hơn với các câu hoàn chỉnh.", interactive=True)
         with gr.Column(scale=1):
-            ljbtn = gr.Button("Synthesize", variant="primary")
-            ljaudio = gr.Audio(interactive=False, label="Synthesized Audio")
+            ljbtn = gr.Button("Tổng hợp giọng nói", variant="primary")
+            ljaudio = gr.Audio(interactive=False, label="Âm thanh tổng hợp")
             ljbtn.click(ljsynthesize, inputs=[ljinp], outputs=[ljaudio], concurrency_limit=4)
 with gr.Blocks(title="StyleTTS 2", css="footer{display:none !important}", theme=theme) as demo:
     gr.Markdown("""# StyleTTS 2
 
-[Paper](https://arxiv.org/abs/2306.07691) - [Samples](https://styletts2.github.io/) - [Code](https://github.com/yl4579/StyleTTS2)
+[Paper](https://arxiv.org/abs/2306.07691) - [Samples](https://styletts2.github.io/) - [Code](https://github.com/yl4579/StyleTTS2) - [Interface](https://twitter.com/realmrfakename)
 
-GUI of StyleTTS 2 by [mrfakename](https://twitter.com/realmrfakename).
-
-#### Help the StyleTTS 2 space get to the top of HF Trending! [Give it a Like!](https://huggingface.co/spaces/styletts2/styletts2)
+Dịch và Triển khai bởi Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội
 
 **Before using this demo, you agree to inform the listeners that the speech samples are synthesized by the pre-trained models, unless you have the permission to use the voice you synthesize. That is, you agree to only use voices whose speakers grant the permission to have their voice cloned, either directly or by license before making synthesized voices public, or you have to publicly announce that these voices are synthesized if you do not have the permission to use these voices.**
 
-**NOTE: StyleTTS 2 does better on longer texts.** For example, making it say "hi" will produce a lower-quality result than making it say a longer phrase.""")
-    gr.TabbedInterface([vctk, clone, lj], ['Multi-Voice', 'Voice Cloning', 'LJSpeech'])
+**LƯU Ý: StyleTTS 2 hoạt động tốt hơn với các đoạn văn bản dài.** Ví dụ, yêu cầu nói "Hello" sẽ cho ra kết quả chất lượng thấp hơn so với việc yêu cầu một cụm từ dài hơn.""")
+    gr.TabbedInterface([vctk, clone, lj], ['Nhiều giọng nói', 'Sao chép giọng nói', 'LJSpeech'])
     gr.Markdown("""
-Demo by [mrfakename](https://twitter.com/realmrfakename). I am not affiliated with the StyleTTS 2 authors.
-
-This is the local version of the demo
+Demo bởi [mrfakename](https://twitter.com/realmrfakename). Dịch và Triển khai bởi Nhóm Nghiên cứu Đổi mới sáng tạo và Chuyển đổi số, Trường Quốc Tế, Đại học Quốc gia Hà Nội
 """)
 if __name__ == "__main__":
     demo.queue(api_open=False, max_size=15).launch(
